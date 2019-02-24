0275f7646627449d7b3c60b49e648b5afb98611b
kimbom <jaruwit.sur@ascendcorp.com>
2019-02-25 00:00:19 +0700

    [Edited] change css color text from green to grey of ProjectList Component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -5,26 +5,26 @@ const ProjectList = () => {
         <div className="project-list section">
 
             <div className="card z-depth-0 project-summary">
-                    <div className="card-content green-text text-darken-3">
+                    <div className="card-content grey-text text-darken-3">
                         <span className="card-title">Project title</span>
                         <p>Posted by kimbom</p>
-                        <p className="green-text">3rd September, 1am</p>     
+                        <p className="grey-text">3rd September, 1am</p>     
                     </div>
             </div>
 
             <div className="card z-depth-0 project-summary">
-                    <div className="card-content green-text text-darken-3">
+                    <div className="card-content grey-text text-darken-3">
                         <span className="card-title">Project title</span>
                         <p>Posted by kimbom</p>
-                        <p className="green-text">3rd September, 1am</p>     
+                        <p className="grey-text">3rd September, 1am</p>     
                     </div>
             </div>
 
             <div className="card z-depth-0 project-summary">
-                    <div className="card-content green-text text-darken-3">
+                    <div className="card-content grey-text text-darken-3">
                         <span className="card-title">Project title</span>
                         <p>Posted by kimbom</p>
-                        <p className="green-text">3rd September, 1am</p>     
+                        <p className="grey-text">3rd September, 1am</p>     
                     </div>
             </div>
 
