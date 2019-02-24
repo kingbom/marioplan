daf3e194a4283c09d8c160b9536c7adb4de8d703
kimbom <jaruwit.sur@ascendcorp.com>
2019-02-24 23:24:52 +0700

    [Edited] html layout Dashboard and Notification

---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,11 @@
+import Reat from 'react'
+
+const Notofications = () => {
+     return (
+         <div>
+             <p>Notification</p>
+         </div>
+     )
+}
+
+export default Notofications
